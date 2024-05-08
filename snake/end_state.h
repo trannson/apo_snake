f39e5d4a74edeee73ef3e4eb2394cf4bee0e372c
Marek Štrympl <strymmar@fel.cvut.cz>
2024-05-08 21:10:08 +0200

    comments in TODO.txt and end_state.h

--- snake/end_state.h
+++ snake/end_state.h
@@ -5,21 +5,32 @@
 
 /*
 * Called when some snake hits boundaries, itself or the other snake
-* Then a screen will apear stating that the snake has died, plus it will show a score
-* When the button is pressed, then the menu will appear again
+* Then a screen will appear stating that the snake has died, plus it will show a score
+* Does have a button, which takes player back to the menu
 *
-*@param Bigsnake: the snake which died
+*@param loser_snake: a snake, that loses
+*@param winner_snake: a snake, that won
+* Note: in single player, loser and winner are the same snake
+*@param multiplayer: true if the game was multiplayer, otherwise false
 */
 void init_screen_state(SnakeBig* loser_snake, SnakeBig* winner_snake, bool multiplayer);
 
 /*
-* Function is called by init_screen_state
-* It draws a text and waits for the green button to be pressed (return to the menu)
+* Function is called by init_screen_state, if the game was singleplayer
+* Text with score will appear
+* Shows two button, one leads back to the menu, the second one enables submiting score with custom name
 *
 *@param big_snake: the snake which died
 */
 void draw_died_screen_single(SnakeBig* big_snake);
 
+/*
+* Draws ending screen for multiplayer game, shows both snake's score
+* Has button, which leads back to the menu
+*
+*@param loser_snake: snake, that lost the game
+*@param winner_snake: snake, that won the game
+*/
 void draw_died_screen_multi(SnakeBig* loser_snake, SnakeBig* winner_snake);
 
 #endif // END_STATE_H
